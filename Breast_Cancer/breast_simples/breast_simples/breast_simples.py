ac22184a63a3238bc84dcc47b08cbd111089ef78
Eduardo Zummach <eduardo.zummach@hotmail.com>
2020-06-14 22:45:24 -0300

    Parte 1

--- Breast_Cancer/breast_simples/breast_simples/breast_simples.py
+++ Breast_Cancer/breast_simples/breast_simples/breast_simples.py
@@ -3,3 +3,20 @@ from sklearn.model_selection import train_test_split
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+
+labels = pd.read_csv('C:\GitHub\DL_Python\Breast_Cancer\labels.csv')
+inputs = pd.read_csv('C:\GitHub\DL_Python\Breast_Cancer\inputs.csv')
+
+inputs_test, inputs_train, lablesl_test, labels_train = train_test_split(inputs, labels, test_size=0.25)
+
+model = Sequential([
+    Dense(units = 16, activation = 'relu', 
+          kernel_initializer = 'random_uniform', 
+          input_dim = 30),
+
+    Dense(units = 1, activation = 'sigmoid')])
+
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+model.fit(inputs_train, labels_train,
+          batch_size = 10,
+          epochs = 100)
